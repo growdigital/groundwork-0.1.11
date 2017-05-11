7007153fb4849598d2673644b29b0f3a67ed587c
Jake Rayson <web@growdigital.org>
2017-05-11 14:14:53 +0100

    building up those lovely svg icons

--- build/assets/scripts.js
+++ build/assets/scripts.js
@@ -1,2 +1,15 @@
-// https://css-tricks.com/test-support-svg-img/
+var supportsSvg =  document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1");
+
+if (supportsSvg) {
+  var url = "assets/modules/components/graphics/icon/icon.svg"
+  var xhr = new XMLHttpRequest();
+
+  xhr.onload = function() {
+    if(xhr.status === 200) {
+      document.getElementById('js-svg').innerHTML = xhr.responseText;
+    }
+  };
+  xhr.open('GET', url, true);
+  xhr.send(null);
+}
 
