dd23189abef77f3b5a33a533e5b190953df0b552
Jake Rayson <web@growdigital.org>
2017-08-06 21:09:26 +0100

    tidied index.js, made favicon grey

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-var Metalsmith  = require('metalsmith');
-var cleancss    = require('metalsmith-clean-css');
-var collections = require('metalsmith-collections');
-var concat      = require('metalsmith-concat');
-var dateFormat  = require('metalsmith-date-formatter');
-var drafts      = require('metalsmith-drafts');
-var feed        = require('metalsmith-feed');
-var ignore      = require('metalsmith-ignore');
-var templates   = require('metalsmith-layouts');
-var markdown    = require('metalsmith-markdown');
-var move        = require('metalsmith-movey').default;
-var partials    = require('metalsmith-discover-partials');
-var permalinks  = require('metalsmith-permalinks');
-var postcss     = require('metalsmith-postcss');
-var metadata    = require('metalsmith-writemetadata');
-var handlebars  = require('handlebars');
+let Metalsmith  = require('metalsmith');
+let cleancss    = require('metalsmith-clean-css');
+let collections = require('metalsmith-collections');
+let concat      = require('metalsmith-concat');
+let dateFormat  = require('metalsmith-date-formatter');
+let drafts      = require('metalsmith-drafts');
+let feed        = require('metalsmith-feed');
+let ignore      = require('metalsmith-ignore');
+let templates   = require('metalsmith-layouts');
+let markdown    = require('metalsmith-markdown');
+let move        = require('metalsmith-movey').default;
+let partials    = require('metalsmith-discover-partials');
+let permalinks  = require('metalsmith-permalinks');
+let postcss     = require('metalsmith-postcss');
+let metadata    = require('metalsmith-writemetadata');
+let handlebars  = require('handlebars');
 
 Metalsmith(__dirname)
   .metadata({
     site: {
-      title: "YourSiteTitle",
+      title: "Blank Groundwork",
       description: "Blank Modular CSS Starter Kit.",
       url: "https://blank.groundwork.rocks/",
       author: "Jake Rayson"
@@ -26,28 +26,28 @@ Metalsmith(__dirname)
   })
   .source('./src')
   .destination('./build')
-  // Only clean in dev. Set to false to preserve fractal styleguide.
+  // Only clean in dev. Set to false to preserve Fractal styleguide.
   .clean(false)
+  // Ignoring documentation and Fractal templates
   .use(ignore([
     'docs/*',
     'assets/modules/**/**/*.hbs',
     'assets/modules/**/**/**/*.hbs',
     'assets/modules/**/**/**/**/*.hbs'
   ]))
-  // Concat: the order is important
+  // Concatenation: the order is important
   // Concat CSS
   .use(concat({
-    files: [ 'assets/css/settings/variables.css'
-            ,'normalize.css/normalize.css'
-            ,'assets/css/settings/base.css'
-            ,'assets/css/settings/typography.css'
-            ,'assets/css/settings/responsive.css'
-            ,'assets/modules/objects/**/*.css'
-            ,'assets/modules/components/**/**/*.css'
-            ,'assets/modules/utilities/**/**/*.css'
-            ,'assets/css/shame.css'
-            // Test CSS, uncomment when you want to test
-            // ,'assets/css/test/*.css'
+    files: [
+       'assets/css/settings/variables.css'
+      ,'normalize.css/normalize.css'
+      ,'assets/css/settings/base.css'
+      ,'assets/css/settings/typography.css'
+      ,'assets/css/settings/responsive.css'
+      ,'assets/modules/objects/**/*.css'
+      ,'assets/modules/components/**/**/*.css'
+      ,'assets/modules/utilities/**/**/*.css'
+      ,'assets/css/shame.css'
     ],
     searchPaths: [ 'node_modules' ],
     output: 'assets/styles.css'
@@ -59,14 +59,15 @@ Metalsmith(__dirname)
   // Concat JavaScript
   .use(concat({
     files: [
-      'assets/js/*.js',
-      'assets/modules/objects/**/*.js',
-      'assets/modules/components/**/**/*.js',
-      'assets/modules/utilities/**/**/*.js'
+       'assets/js/*.js'
+      ,'assets/modules/objects/**/*.js'
+      ,'assets/modules/components/**/**/*.js'
+      ,'assets/modules/utilities/**/**/*.js'
     ],
     searchPaths: [ 'node_modules' ],
     output: 'assets/scripts.js'
   }))
+  // +1 PostCSS for vanilla CSS. Use CSS preprocessor of your choice!
   .use(postcss({
     plugins: {
       'postcss-cssnext': {}
@@ -80,10 +81,13 @@ Metalsmith(__dirname)
       reverse: true
     }
   }))
-  // For debugging, use metadata
+
+  // To help with debugging, use metadata
   // .use(metadata({
   //   pattern: ['*.md', '*.html']
   // }))
+
+  // Use GitFriendlyMarkdown formatter
   .use(markdown({
     gfm: true
   }))
@@ -91,7 +95,10 @@ Metalsmith(__dirname)
     directory: 'partials',
     pattern: /\.hbs$/
   }))
+  // Create RSS feed
   .use(feed({collection: 'posts'}))
+  // Change your date format here
+  // Uses Moment.js http://momentjs.com/docs/#/displaying/
   .use(dateFormat({
     key: 'date',
     format: 'ddd D MMM YYYY'
@@ -100,9 +107,11 @@ Metalsmith(__dirname)
     pattern: './posts/:title',
     relative: false
   }))
+  // I like Handlebars templating. You can use what you like.
   .use(templates({
     engine: 'handlebars'
   }))
+  // Move graphics assets out of modules and into /assets/images/ directory
   .use(move({
     'assets/modules/components/**/**/*.+(png|svg|ico|jpg)': 'assets/images/{name}{ext}'
   }))
